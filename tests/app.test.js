5c9df8cd31e6947bb984939e4d9f7576a548b38f
ValeriyPetryna <valeriypetrina@gmail.com>
2019-05-06 17:26:07 +0300

    change expect

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,16 +2,13 @@ const {list} = require('../app')
 
 describe('Winner list', () => {
   it('list from app.js should be the same as expected listOfWinners', () => {
-      
     const expected = [
       '1. sidrov with combo: Quadro, kicker: 4',
       '2. ivanov1 with combo: Triple, kicker: 2',
       '3. petrov with combo: High Card, kicker: 13',
       '4. ivanov2 with combo: High Card, kicker: 11'
     ]
-    for (let i = 0; i < list.length; i++){
-      expect(list[i]).to.equal(expected[i])
-    }
-
+    
+      expect(list).to.be.deep.equal(expected)
   })
 })
